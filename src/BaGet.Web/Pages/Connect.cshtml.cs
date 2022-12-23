48f9ee39f48533e5da28d6d9e744635d4187e248
Sebastian Caliebe <caliebe@in-reach.de>
2022-12-23 22:08:21 +0100

    Connect to feed site added

---
+++ src/BaGet.Web/Pages/Connect.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace BaGet.Web.Pages
+{
+    public class ConnectModel : PageModel
+    {
+        public readonly IHttpContextAccessor _currentContextAccessor;
+        private readonly IConfiguration _configuration;
+
+        public ConnectModel(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _currentContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!_currentContextAccessor.HttpContext.User.Claims
+                .Any(x => x.Type == "BaGet") ||
+                _currentContextAccessor.HttpContext.User.Claims.
+                Single(x => x.Type == "BaGet").Value != _configuration["ApiKey"])
+            {
+                return RedirectToPage("Login");
+            }
+
+            return Page();
+        }
+    }
+}
